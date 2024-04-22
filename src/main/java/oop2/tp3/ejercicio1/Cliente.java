9bd97ccd0e818378574cb99506a5fe6cef955a80
javier amaya <amayajavier913@gmail.com>
2024-04-22 17:45:38 -0300

    Initial commit

---
+++ src/main/java/oop2/tp3/ejercicio1/Cliente.java
@@ -0,0 +1,32 @@
+package oop2.tp3.ejercicio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    private List<Alquiler> alquileres = new ArrayList<Alquiler>();
+    private String name;
+
+    public Cliente(String nombre) {
+        this.name = nombre;
+    }
+
+    public Object[] calcularDeudaYPuntosObtenidos() {
+        Object[] resultado = new Object[2];
+        double total = 0;
+        int puntosAlquilerFrecuente = 0;
+        for (Alquiler alquiler : alquileres) {
+            total = alquiler.copia().libro().calcularCosto(alquiler.diasAlquilados());
+            puntosAlquilerFrecuente++; // sumo puntos por alquiler
+            // bonus por dos días de alquiler de un nuevo lanzamiento
+            puntosAlquilerFrecuente+=alquiler.copia().libro().calcularBonus(alquiler.diasAlquilados());
+        }
+        resultado[0] = total;
+        resultado[1] = puntosAlquilerFrecuente;
+        return resultado;
+    }
+
+    public void alquilar(Alquiler rental) {
+        alquileres.add(rental);
+    }
+}
---
+++ src/main/java/oop2/tp3/ejercicio1/Libro.java
@@ -0,0 +1,29 @@
+package oop2.tp3.ejercicio1;
+
+public abstract class Libro {
+//    public static final int INFANTILES = 2;
+//    public static final int REGULARES = 0;
+//    public static final int NUEVO_LANZAMIENTO = 1;
+    private String nombre;
+    private int codigoPrecio;
+
+    public Libro(String nombre) {
+        this.nombre = nombre;
+    }
+
+//    public int codigoPrecio() {
+//        return codigoPrecio;
+//    }
+
+    public String nombre() {
+        return nombre;
+    }
+
+    public double calcularCosto(int diasAlquilados) {
+        return 0;
+    }
+
+    public int calcularBonus(int diasAlquildos) {
+        return 0;
+    }
+}
